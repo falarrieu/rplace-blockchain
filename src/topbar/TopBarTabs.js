645bbb86695f089fc936bb47616a97f10028a383
Federico Larrieu <federicolarrieu@outlook.com>
2023-04-16 12:46:35 -0600

    Tabs

---
+++ src/topbar/TopBarTabs.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {Tab, Tabs} from "@mui/material";
+import styled from "@emotion/styled";
+import PaletteIcon from '@mui/icons-material/Palette';
+import InfoIcon from '@mui/icons-material/Info';
+import WalletIcon from '@mui/icons-material/Wallet';
+
+const AlignedTabs = styled(Tabs)(() => ({
+	"& .MuiTab-root": {
+        display:"flex",
+		justifyContent: 'start', 
+		minWidth:0, 
+		minHeight: '48px',
+		paddingLeft:'8px',
+		whiteSpace: 'nowrap',
+	},
+	"& button:focus": { 
+		outline: "none"
+	},
+    "& .MuiTabs-flexContainer": {
+        display:"flex",
+		justifyContent: 'center', 
+	},
+}));
+
+export default function TopBarTabs({expanded, tabsContext}){
+
+	return (
+		<AlignedTabs
+			orientation="horizontal"
+			variant="standard"
+			value={tabsContext.tab}
+			onChange={(e,newTab) => tabsContext.openTab(newTab)}
+		>
+            <Tab 
+                value={"Canvas"}
+                label={expanded ? "Canvas" : ""} 
+				style={{ color: "rgb(255 106 0)" }}
+                icon={<PaletteIcon style={{ color: "rgb(90 90 90)" }}/>} 
+                iconPosition="start" 
+            />
+            <Tab 
+                value={"About"}
+                label={expanded ? "About" : ""} 
+				style={{ color: "rgb(255 106 0)" }}
+                icon={<InfoIcon style={{ color: "rgb(90 90 90)" }}/>} 
+                iconPosition="start" 
+            />
+			<Tab 
+                value={"Wallet"}
+                label={expanded ? "Wallet" : ""} 
+				style={{ color: "rgb(255 106 0)" }}
+                icon={<WalletIcon style={{ color: "rgb(90 90 90)" }}/>} 
+                iconPosition="start" 
+            />
+		</AlignedTabs>
+	);
+
+}
