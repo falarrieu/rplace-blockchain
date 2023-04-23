bcc78d0415a9632a59a04e5da7ad6967fc1a74d8
falarrieu <federicolarrieu@outlook.com>
2023-04-22 20:43:14 -0600

    Refactor and ChangePixel Function

--- src/canvas/PixelCanvas.js
+++ src/canvas/PixelCanvas.js
@@ -2,46 +2,49 @@ import React, {useEffect, useState} from 'react';
 import DeckGL from '@deck.gl/react';
 import {SolidPolygonLayer} from '@deck.gl/layers';
 import useViews from "../hooks/useViews";
-import useData from "../hooks/useData";
+import TransactionPane from "./TransactionPane";
+import useTransaction from "../hooks/useTransaction";
 
 /**
  * Component that encapsulated the rendering of the canvas.
  * @param  {Callback} setLoading Sets the loading state.
  * @return {DeckGL}      The DeckGL rendering component
  */
-function PixelCanvas({setLoading}) {
+function PixelCanvas({canvasContext,loadingContext, contractContext}) {
 
     const viewContext = useViews();
-    const dataContext = useData(setLoading);
-
+    const transactionContext = useTransaction(canvasContext, contractContext);
     const [viewState, setViewState] = useState({target: [5000, 5000], zoom: -3.5})
-
-    useEffect(() => {
-        setLoading(true);
-    }, [])
 
     const pixelLayer = new SolidPolygonLayer({
         id:"pixelLayer",
-        data: dataContext.data,
+        data: canvasContext.data,
         getPolygon: d => d.polygon,
         getFillColor: d => d.color,
+        onClick: d => transactionContext.openTransaction(d),
         pickable: true,
         autoHighlight: true,
         highlightColor: d => [250,250,250, 150],
+        updateTriggers: {
+            getFillColor: d => d.color
+        }
         // extruded: true,  To be implemented with the 3D viewer (GPU Heavy)
         // getElevation: d => d.numPurchases
     })
 
     return (
-        <DeckGL
-            viewState={viewState}
-            onViewStateChange={e => {
-                setViewState(e.viewState)}
-            }
-            views={viewContext.views}
-            controller={true}
-            layers={pixelLayer}
-        />
+        <>
+            {transactionContext.transactionPaneOpen ? <TransactionPane transactionContext={transactionContext}/> : null}
+            <DeckGL
+                viewState={viewState}
+                onViewStateChange={e => {
+                    setViewState(e.viewState)}
+                }
+                views={viewContext.views}
+                controller={true}
+                layers={pixelLayer}
+            />
+        </>
     );
 }
 
